6c34ec1d1ea86546be4f87ef1dc4fb1425d37d21
novikov <cto@capusta.space>
2020-09-28 14:19:20 +0300

    v1.7.0 CPME-1648 billStatus transactionId

--- src/Model/Response/Status/GetStatusResponse.php
+++ src/Model/Response/Status/GetStatusResponse.php
@@ -6,7 +6,9 @@ namespace Capusta\SDK\Model\Response\Status;
 use Capusta\SDK\Model\Interfaces\RestorableInterface;
 use Capusta\SDK\Model\Request\Item\SenderRequestItem;
 use Capusta\SDK\Model\Response\AbstractResponse;
+use Capusta\SDK\Model\Response\Item\AmountItem;
 use Capusta\SDK\Model\Response\Payment\CreatePaymentResponse;
+use Capusta\SDK\Model\Types\AbstractCustomType;
 
 class GetStatusResponse extends CreatePaymentResponse
 {
@@ -35,22 +37,36 @@ class GetStatusResponse extends CreatePaymentResponse
         return $this->transactions;
     }
 
+    public function getRequiredFields()
+    {
+        return [
+            'id' => AbstractResponse::TYPE_STRING,
+            'transactionId' => AbstractResponse::TYPE_STRING,
+            'partnerPaymentId' => AbstractResponse::TYPE_STRING,
+            'amount' => AmountItem::class,
+            'status' => AbstractResponse::TYPE_STRING,
+            'projectId' => AbstractResponse::TYPE_INTEGER,
+            'projectCode' => AbstractResponse::TYPE_STRING,
+            'payUrl' => AbstractResponse::TYPE_STRING,
+            'sender' => SenderRequestItem::class,
+            'multiBill' => AbstractResponse::TYPE_BOOLEAN,
+            'billPaymentEnabled' => AbstractResponse::TYPE_BOOLEAN,
+            'created_at' => RestorableInterface::TYPE_DATE
+        ];
+    }
     /**
      * @inheritDoc
      */
     public function getOptionalFields()
     {
         return [
-            'description' => AbstractResponse::TYPE_STRING,
-            'projectId' => AbstractResponse::TYPE_INTEGER,
             'sender' => SenderRequestItem::class,
-            'contentUrl' => RestorableInterface::TYPE_STRING,
-            'updated_at' => RestorableInterface::TYPE_DATE,
             'expire' => RestorableInterface::TYPE_DATE,
             'custom' => AbstractResponse::TYPE_STRING,
-            'transactions' => AbstractResponse::TYPE_ARRAY,
-            'multiBill' => AbstractResponse::TYPE_BOOLEAN,
-            'billPaymentEnabled' => AbstractResponse::TYPE_BOOLEAN,
+            'description' => AbstractResponse::TYPE_STRING,
+            'contentUrl' => RestorableInterface::TYPE_STRING,
+            'updated_at' => RestorableInterface::TYPE_DATE,
+            'transactions' => AbstractResponse::TYPE_ARRAY
         ];
     }
 }
